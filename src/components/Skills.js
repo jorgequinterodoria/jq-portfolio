ec839c4d5247cc9ea0a1f595d21bd82d1216dfac
jorgequintero <jquintedori@gmail.com>
2023-12-05 16:12:11 -0500

    fix

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,8 +3,6 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -38,19 +36,19 @@ export const Skills = () => {
               <p>A short write-up of my skills<br></br></p>
               <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="logo front" />
                   <h5>Frontend Development</h5>
                 </div>
                 <div className="item">
-                  <img src={meter2} alt="Image" />
+                  <img src={meter2} alt="logo back" />
                   <h5>Backend Development</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image" />
+                  <img src={meter3} alt="graphQL" />
                   <h5>GraphQL</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="Agile" />
                   <h5>Agile</h5>
                 </div>
               </Carousel>
@@ -58,7 +56,7 @@ export const Skills = () => {
           </div>
         </div>
       </div>
-      <img className="background-image-left" src={colorSharp} alt="Image" />
+      <img className="background-image-left" src={colorSharp} alt="color sharp" />
     </section>
   )
 }
